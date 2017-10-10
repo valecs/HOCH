64d98357185bf1a3d2f1dbf6ee3973eead80751f
Vale Cofer-Shabica <vale@brown.edu>
2017-10-10 14:32:51 -0400

    added new tooling for finishing on ted

---
+++ HOCH_Geodesics_MPI/jobs/mixed-ted/submit.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -euo pipefail
+
+readonly f_name=$1
+
+readonly exe=$(readlink -f ../../../shared/bin/HOCH_Geodesics_MPI)
+
+if [ ! -f "$f_name" ]
+then
+    echo "$f_name is not a file; exiting!"
+    exit 1
+fi
+
+shift 1
+
+readonly F=$(readlink -f "$f_name")
+
+readonly out_name="./${f_name}_o"
+mkdir "$out_name"
+
+readonly out=$(readlink -f "$out_name")
+
+readonly N=$(wc -l < "$F")
+
+echo "submitting $F"
+echo "writing to $out"
+echo "$N tasks"
+
+sleep 3s
+
+qsub -b yes -shell no -j yes -o "$out" -wd "$out" -N "HOCH$f_name" -p -1 -q stratt,chemistry -tc 120 \
+     -v LD_LIBRARY_PATH="$HOME/toor/lib:$HOME/toor/gcc62/lib64:/share/apps/lib",STRATT_ROOT="$STRATT_ROOT" \
+     -t "1-$N" "$@" "$exe" -G -F "$F"
