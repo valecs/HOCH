6b23992cb3c4e556c31a607a24eb2c43f0203879
Vale Cofer-Shabica <vale@brown.edu>
2017-12-07 14:39:13 -0500

    restarting db

--- HOCH_Geodesics_MPI/jobs/mixed-ted/submit.sh
+++ HOCH_Geodesics_MPI/jobs/mixed-ted/submit.sh
@@ -17,7 +17,7 @@ shift 1
 readonly F=$(readlink -f "$f_name")
 
 readonly out_name="./${f_name}_o"
-mkdir "$out_name"
+mkdir "$out_name" || true
 
 readonly out=$(readlink -f "$out_name")
 
@@ -29,6 +29,6 @@ echo "$N tasks"
 
 sleep 3s
 
-qsub -b yes -shell no -j yes -o "$out" -wd "$out" -N "HOCH$f_name" -p -1 -q stratt,chemistry -tc 120 \
+qsub -b yes -shell no -j yes -o "$out" -wd "$out" -N "j$f_name" -p -1 -q stratt,chemistry,gaussian,doll -tc 140 \
      -v LD_LIBRARY_PATH="$HOME/toor/lib:$HOME/toor/gcc62/lib64:/share/apps/lib",STRATT_ROOT="$STRATT_ROOT" \
      -t "1-$N" "$@" "$exe" -G -F "$F"
